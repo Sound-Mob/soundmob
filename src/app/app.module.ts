a4ea384c56a72c14a63efbbe2df22286eead7fdf
jvalamis <jvalamis@gmail.com>
2018-10-01 13:25:07 -0700

    double quotes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from "@angular/forms";
 import {HttpModule} from '@angular/http';
 
 import { AppComponent } from "./app.component";
-import {HomeComponent} from './home/home.component';
+import {HomeComponent} from "./home/home.component";
 
 import { AppRoutingModule } from ".//app-routing.module";
 
