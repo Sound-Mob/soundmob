976decc7472546213e82a8f6296a744803c97b27
Connor <jmconnor3@gmail.com>
2018-10-01 23:30:40 -0500

    'connected to remote db'

--- server/database.js
+++ server/database.js
@@ -2,10 +2,11 @@ const pgp = require('pg-promise')();
 
 // define db params
 const cn = {
-  host: 'localhost', // server name or IP address;
+  host: 'elmer.db.elephantsql.com', // server name or IP address;
   port: 5432,
-  database: 'soundmob',
-  user: 'josephdelahoussaye'
+  database: 'rvihxahs',
+  user: 'rvihxahs',
+  password: 
 };
 const db = pgp(cn); // database instance;
 // database methods
@@ -20,12 +21,16 @@ module.exports = {
         console.log(error); // print the error;
       }),
   // select and return users
-  getUsers: ()=>
-    db.any('SELECT name FROM users')
+  getUsers: ()=> {
+   return  db.any('SELECT name FROM users')
       .then(user => {
         console.log(user); // print user name;
       })
       .catch(error => {
         console.log(error); // print the error;
-      }),
+      });
+  },
+  getUserById: (id) => {
+   return db.any(`SELECT * FROM users WHERE id (${id})`)
+  }
 };
