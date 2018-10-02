2b13cd3cef493bfa54d0b91f30853d0f96995574
Connor <jmconnor3@gmail.com>
2018-10-01 23:29:52 -0500

    'set up new outh'

--- server/app.js
+++ server/app.js
@@ -1,10 +1,39 @@
 const express = require('express');
 const path = require('path');
-const { createUser, getUsers } = require('./database');
+const { createUser, getUsers, getUserById } = require('./database');
 const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
-const app = express();
 const passport = require('passport');
 const { Youtube, ClientID, ClientSecret, RedirectURL} = require('./config.js');
+const app = express();
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  console.log(user);
+  done(null, user.id); 
+ // where is this user.id going? Are we supposed to access this anywhere?
+});
+
+passport.deserializeUser((id, done)=> {
+getUserById(id).then((res, user) => {
+  done(null,user)
+}).catch( err => console.error(err))
+ });
+// passport.serializeUser( (user, done) => {
+//   var sessionUser = { _id: user._id, name: user.name, email: user.email, roles: user.roles }
+//   done(null, sessionUser)
+// })
+
+// passport.deserializeUser( (sessionUser, done) => {
+//   // The sessionUser object is different from the user mongoose collection
+//   // it's actually req.session.passport.user and comes from the session collection
+//   done(null, sessionUser)
+// })
 passport.use(new GoogleStrategy({
   clientID:     ClientID,
   clientSecret: ClientSecret,
@@ -12,11 +41,19 @@ passport.use(new GoogleStrategy({
   passReqToCallback   : true
 },
 function(request, accessToken, refreshToken, profile, done) {
-  console.log(profile);
-done();
+  // console.log(accessToken,'token', refreshToken,'refresh')
+  // console.log(request);
+done(null, profile);
 
 }
+
 ));
+
+
+// used to deserialize the user
+
+
+
 
 app.get('/',
 passport.authenticate('google', { scope: 
