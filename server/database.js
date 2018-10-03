165f9d9ba3de190981222157064e09db364f3c82
Connor <jmconnor3@gmail.com>
2018-10-03 08:31:59 -0500

    'have to refactor User check/create function'

--- server/database.js
+++ server/database.js
@@ -11,11 +11,16 @@ const cn = {
 
 const db = pgp(cn); // database instance;
 // database methods
+/* TODO's
+* a function to save playlist
+* refactor function for checking users/ creating users
+*/
 module.exports = {
   // adds a user to the user database
   createUser: (googleid, firstname, lastname, bio, samples, savedplaylists, followercount, followingcount) => {
     return db.any('INSERT INTO users (googleid, firstname, lastname, bio, samples, savedplaylists, followercount, followingcount) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', [googleid, firstname, lastname, bio, samples, savedplaylists, followercount, followingcount]);
   },
+  
   // select and return users
   getUsers: ()=> {
    return  db.any('SELECT name FROM users')
@@ -27,6 +32,6 @@ module.exports = {
       });
   },
   getUserById: (id) => {
-   return db.any(`SELECT * FROM users WHERE googleid = $1`,[id])
+   return db.any(`SELECT * FROM users WHERE googleid = $1`,id)
   }
 };
