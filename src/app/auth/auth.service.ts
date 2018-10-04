ca617ad3e6707df46dab43412225dc3cf16da98c
kennetholvr <kennetholvr@gmail.com>
2018-10-03 22:30:32 -0500

    commit to fix

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+
+  get isLoggedIn() {
+    return false;
+  }
+
+  get isSuperAdmin() {
+    return true;
+  }
+
+}
