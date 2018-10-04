5a97e732fb86caf364799719fe3fca1f530c7a86
Connor <jmconnor3@gmail.com>
2018-10-03 19:43:30 -0500

    'added searchSong, retreive playlist, create playlist'

--- server/util.js
+++ server/util.js
@@ -1,3 +1,5 @@
+const rp = require('request-promise');
+
 /*
 housing for utility functions
 
@@ -7,3 +9,46 @@ housing for utility functions
 * a function to  pull the playlist into the room
 *
 */
+module.exports = {
+  //TODO make the token dynamic
+   playlist: (token) => {
+    const options = {
+      uri: 'https://www.googleapis.com/youtube/v3/playlists?part=snippet&mine=true',
+      headers: {
+          'User-Agent': 'Request-Promise',
+          Authorization: `Bearer ${token}`
+      },
+      json: true // Automatically parses the JSON string in the response
+  };
+  return rp(options)
+
+  },
+  createPlaylist: (token, obj) => {
+    const options = {
+      method: 'POST',
+      uri: 'https://www.googleapis.com/youtube/v3/playlists?part=snippet',
+      body: {
+          "snippet": {
+            "title": "New playlist", 
+            "description": "Sample playlist for Data API" }
+       },
+       headers:{
+        Authorization: `Bearer ${token}`
+       },
+       json: true
+      };
+      return rp(options);
+  },
+  searchSong: (token, song) => {
+    const options = {
+        uri:`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${song}&type=video`,
+        headers: {
+          'User-Agent': 'Request-Promise',
+          Authorization: `Bearer ${token}`
+      },
+      json: true
+    };
+    return rp(options);
+  }
+
+  }
