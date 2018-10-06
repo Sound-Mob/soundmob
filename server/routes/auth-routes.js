1b0280379eb439456d2e87384e4f6e3a0383e9ec
Connor <jmconnor3@gmail.com>
2018-10-06 17:32:01 -0500

    'changed redirect route'

--- server/routes/auth-routes.js
+++ server/routes/auth-routes.js
@@ -12,7 +12,7 @@ router.get('/',
 
 router.get( '/google/callback', 
   passport.authenticate('google',{
-    successRedirect:'http://localhost:8080/posts',
+    successRedirect:'/',
     failureRedirect:'/auth'
   }) );
 module.exports = router;
