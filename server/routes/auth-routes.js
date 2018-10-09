fc70c193a657286446abee61fa61eb841bc9ddb8
Connor <jmconnor3@gmail.com>
2018-10-09 11:58:10 -0500

    'update auth'

--- server/routes/auth-routes.js
+++ server/routes/auth-routes.js
@@ -1,18 +1,19 @@
 const router = require('express').Router();
 const passport = require('passport');
-
 router.get('/',
   passport.authenticate('google', { scope: 
-  [ 'https://www.googleapis.com/auth/plus.login',
+  [ 
+    'https://www.googleapis.com/auth/plus.login',
     'https://www.googleapis.com/auth/youtube',
     'https://www.googleapis.com/auth/plus.me',
     'https://www.googleapis.com/auth/userinfo.email',
-    'https://www.googleapis.com/auth/youtube.force-ssl' ]  }
+    'https://www.googleapis.com/auth/youtube.force-ssl' 
+  ]  }
   ));
 
-router.get( '/google/callback', 
+router.get( '/google/callback',
   passport.authenticate('google',{
-    successRedirect:'http://localhost:8080/posts',
+    successRedirect:'/',
     failureRedirect:'/auth'
   }) );
 module.exports = router;
