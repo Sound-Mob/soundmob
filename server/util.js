60c9ed73eb842940e6bf4d8bb02e1a0dada146a1
Connor <jmconnor3@gmail.com>
2018-10-10 18:54:59 -0500

    'added getSoundBoard Method'

--- server/util.js
+++ server/util.js
@@ -10,57 +10,56 @@ housing for utility functions
 *
 */
 module.exports = {
-  //TODO make the token dynamic
-   playlist: (token) => {
+// TODO make the token dynamic
+  playlist: (token) => {
     const options = {
       uri: 'https://www.googleapis.com/youtube/v3/playlists?part=snippet&mine=true',
       headers: {
-          'User-Agent': 'Request-Promise',
-          Authorization: `Bearer ${token}`
+        'User-Agent': 'Request-Promise',
+        Authorization: `Bearer ${token}`,
       },
-      json: true // Automatically parses the JSON string in the response
-  };
-  return rp(options)
-
+      json: true, // Automatically parses the JSON string in the response
+    };
+    return rp(options);
   },
   createPlaylist: (token, obj) => {
     const options = {
       method: 'POST',
       uri: 'https://www.googleapis.com/youtube/v3/playlists?part=snippet',
       body: {
-          "snippet": {
-            "title": "New playlist", 
-            "description": "Sample playlist for Data API" }
-       },
-       headers:{
-        Authorization: `Bearer ${token}`
-       },
-       json: true
-      };
-      return rp(options);
+        snippet: {
+          title: 'New playlist',
+          description: 'Sample playlist for Data API',
+        },
+      },
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      json: true,
+    };
+    return rp(options);
   },
   searchSong: (token, song) => {
     const options = {
-        uri:`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${song}&type=video`,
-        headers: {
-          'User-Agent': 'Request-Promise',
-          Authorization: `Bearer ${token}`
+      uri: `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${song}&type=video`,
+      headers: {
+        'User-Agent': 'Request-Promise',
+        Authorization: `Bearer ${token}`,
       },
-      json: true
+      json: true,
     };
     return rp(options);
   },
   searchDetails: (token, id) => {
-    console.log({id});
-    
+    console.log({ id });
     // id = '9bZkp7q19f0';
     const options = {
       uri: `https://www.googleapis.com/youtube/v3/videos?id=${id}&part=contentDetails`,
       headers: {
         'User-Agent': 'Request-Promise',
-        Authorization: `Bearer ${token}`
+        Authorization: `Bearer ${token}`,
       },
-      json: true
+      json: true,
     };
     return rp(options);
   },
@@ -68,58 +67,65 @@ module.exports = {
     const options = {
       method: 'POST',
       uri: 'https://www.googleapis.com/youtube/v3/playlists?part=snippet',
-      body:   {
-        "snippet": {
-          "playlistId": "PLR4epTa34lZwzBUMi10kEArbGLhwJ9fv6", 
-          "resourceId": {
-              "kind": "youtube#video",
-              "videoId": "74pZUgCOJoE"
-            }
-       
-          }
-       },
-       headers:{
-        Authorization: `Bearer ${token}`
-       },
-       json: true
-      };
-      return rp(options);
+      body: {
+        snippet: {
+          playlistId: 'PLR4epTa34lZwzBUMi10kEArbGLhwJ9fv6',
+          resourceId: {
+            kind: 'youtube#video',
+            videoId: '74pZUgCOJoE',
+          },
+        },
+      },
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      json: true,
+    };
+    return rp(options);
   },
-  playlistIDs: (token)  =>{
-    
-
+  playlistIDs: (token) => {
     const options = {
       uri:`https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&playlistId=PLR4epTa34lZzzIbqRVWGRdtbpB_UlvMdl`,
       headers: {
         'User-Agent': 'Request-Promise',
-        Authorization: `Bearer ${token}`
-    },
-    json: true
-  };
-  return rp(options);
+        Authorization: `Bearer ${token}`,
+      },
+      json: true,
+    };
+    return rp(options);
   },
   videoIDArray: (array) => {
-     return array.map(item => {
-        const {contentDetails} = item;
-         const {videoId} = contentDetails;
-         return videoId
-      });
+    return array.map((item) => {
+      const { contentDetails } = item;
+      const { videoId } = contentDetails;
+      return videoId;
+    });
   },
   searchDetailsArray: (array, token) => {
-  
-    
     const options = {
-      uri: `https://www.googleapis.com/youtube/v3/videos?part=contentDetails&id=`,
+      uri: 'https://www.googleapis.com/youtube/v3/videos?part=contentDetails&id=',
       headers: {
         'User-Agent': 'Request-Promise',
-        Authorization: `Bearer ${token}`
+        Authorization: `Bearer ${token}`,
       },
-      json: true
+      json: true,
     };
-    array.forEach(id => {
-      options.uri += `${id}`
-    })
+    array.forEach((id) => {
+      options.uri += `${id}`;
+    });
     return rp(options);
+  },
+  getSoundBoard: (token) => {
+    const options = {
+      uri: 'https://www.googleapis.com/storage/v1/b/djtannertestbucket/o',
+      headers: {
+        'User-Agent': 'Request-Promise',
+        Authorization: `Bearer ${token}`,
+      },
+      json: true,
+    };
+    return rp(options);
+
   }
 
   }
