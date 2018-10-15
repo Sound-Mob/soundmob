4def20571b2dabae2635f470aa3477a7413d7ba7
JoeyDelahoussaye <jldela@gmail.com>
2018-10-14 20:28:43 -0500

    single song sync listner w dj

--- src/app/listener/soundplayer/soundplayer.component.ts
+++ src/app/listener/soundplayer/soundplayer.component.ts
@@ -28,7 +28,6 @@ export class SoundplayerComponent implements OnInit {
     this.chatService.listenerReceiveSongDetails()
       .subscribe(songinfo => {
        
-        
         let startAt = songinfo['listenerStartTime'] - songinfo['songinfo'][0].starttime;
         // console.log(startAt, " start time ready for vid");
         this.video = `https://www.youtube.com/embed/${songinfo['songinfo'][0].songid}?start=${startAt}&rel=0&modestbranding=1&autohide=1&mute=0&showinfo=0&controls=0&autoplay=1`
