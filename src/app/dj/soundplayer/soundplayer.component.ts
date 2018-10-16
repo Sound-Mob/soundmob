6dbd3b732ab87fd82728d31298d4d8aa0f7a6a38
JoeyDelahoussaye <jldela@gmail.com>
2018-10-15 20:45:35 -0500

    adding playlist and songs to it

--- src/app/dj/soundplayer/soundplayer.component.ts
+++ src/app/dj/soundplayer/soundplayer.component.ts
@@ -11,9 +11,10 @@ export class SoundplayerComponent implements OnInit {
   items: any;
   sounds: Array<{ name: string, mediaLink: string }> = [];
   playlists: Array<{ name: string, id: string }> = [];
+  newcastid: string;
+  searchResults: Array<{ name: string, id: string }> = [];
+  castName: string:
 
-
-  
   constructor(private http: HttpClient, private soundBite:SoundBoardService) { }
   ngOnInit() {
       this.http.get('/test')
@@ -36,7 +37,32 @@ export class SoundplayerComponent implements OnInit {
   playlistClick(event) {
     this.soundBite.playlistEmit(event.target.id);
   }
+  openNewCastComponent() {
+    console.log("this will open the entire new cast creator")
+  }
+  searchSongToCast(song){
+    this.http.post('djView/searchSong', { song }).subscribe((data) => {
+      console.log(data);
+      this.searchResults = data['items'].map((songObj)=>{
+        return { name: songObj.snippet.title, id: songObj.id.videoId }
+      })
+    })
+  }
 
+  songSelect(event){
+    console.log(event.target.id);
+    this.http.post('/djView/insertSong', { songId: event.target.id, playlistId: this.newcastid, title: this.castName })
+    .subscribe((data)=>{
+      console.log(data);
+    })
+  }
+
+  submitCastName(title){
+    this.castName = title;
+    this.http.post('djView/nameCast',{title}).subscribe((data)=>{
+      this.newcastid = data['id'];
+    })
+  }
    soundClick(event) {
     //  console.log(event.target.id);
      this.soundBite.soundEmit(event.target.id);
