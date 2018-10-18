c5c5d9ef71de8eb15d575155dae90b854b50a6c1
Connor <jmconnor3@gmail.com>
2018-10-18 12:41:28 -0500

    'using new auth service'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AdminGuard } from "./auth/guards/admin.guard";
 import { LandingComponent } from "./home/landing/landing.component";
-
+import { NeedAuthGuardService } from './need-auth-guard.service'
+ 
 
 const routes: Routes = [
   {
     path: '',
+    loadChildren: "../app/featured/featured.module#FeaturedModule",
+    canActivate: [NeedAuthGuardService]
+  },
+  {
+    path: 'login',
     component: LandingComponent,
+
   },
   {
     path: "featured",
