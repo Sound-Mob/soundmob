ea6018ffdfb33207299c89caf125b82959169c18
jvalamis <jvalamis@gmail.com>
2018-10-19 14:21:48 -0500

    landing page css and background change, header color change, footer added, the white i'm using is ghost white #f8f8ff

--- buildScript.sh
+++ buildScript.sh
@@ -2,10 +2,10 @@
 
 # Build a new docker image
 # do NOT use tags, then push WITH latest tag !important
-docker build -t us.gcr.io/sound-mob12282/soundmob:11.32 .
+docker build -t us.gcr.io/sound-mob12282/soundmob:12.20 .
 
 # Push new docker image to gcloud
-docker push us.gcr.io/sound-mob12282/soundmob:11.32
+docker push us.gcr.io/sound-mob12282/soundmob:12.20
 
 
 # Update running compute engine with newest image
@@ -21,7 +21,7 @@ docker push us.gcr.io/sound-mob12282/soundmob:11.32
 #certbot shit, this will give us a cert that lasts 3 months
 # certbot -d soundmob.net --manual --logs-dir certbot --config-dir certbot --work-dir certbot --server https://acme-v02.api.letsencrypt.org/directory --email jvalamis@gmail.com --preferred-challenges dns certonly
 
-#checking if domain 
+#checking if domain
 #dig @8.8.8.8 soundmob.systems
 #host -t txt soundmob.systems
 #nslookup -type=txt soundmob.systems
