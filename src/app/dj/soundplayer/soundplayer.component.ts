19011439d18240ac191720ab02f402a7b63c44ae
JoeyDelahoussaye <jldela@gmail.com>
2018-10-20 14:11:15 -0500

    fixed sound samples firing

--- src/app/dj/soundplayer/soundplayer.component.ts
+++ src/app/dj/soundplayer/soundplayer.component.ts
@@ -105,8 +105,8 @@ constructor(private http: HttpClient, private soundBite:SoundBoardService, priva
     })
   }
    soundClick(event) {
-    //  console.log(event.target.id);
-     this.soundBite.soundEmit(event.path[2].id);
+     console.log(event);
+     this.soundBite.soundEmit(event.path[1].id);
     
    }
 
