05fcf222f1ab73698834e57aa8285c5f8bde8dbe
Connor <jmconnor3@gmail.com>
2018-10-25 11:46:14 -0500

    ' added chat service implemented method in guard'

--- src/app/need-auth-guard.service.ts
+++ src/app/need-auth-guard.service.ts
@@ -4,18 +4,19 @@ import {CanActivate, Router} from '@angular/router';
 
 import {ActivatedRouteSnapshot, RouterStateSnapshot} from '@angular/router/src/router_state';
 import { AuthService } from '../app/auth/auth.service';
+import { ChatService } from './services/chat.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NeedAuthGuardService implements CanActivate {
 
-  constructor(private router: Router, private auth: AuthService) { }
+  constructor(private router: Router, private auth: AuthService, private chat: ChatService) { }
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     const redirectUrl = route['_routerState']['url']; 
       
-      if(localStorage.getItem('clicked')) {
-        localStorage.clear();
+      if(document.cookie) {  
+        this.chat.init();
         return true
       } 
     this.router.navigateByUrl(
