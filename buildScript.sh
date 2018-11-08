0e6c86e8c788a398ed74799edb6e0e5c607a6463
jvalamis <jvalamis@gmail.com>
2018-11-08 15:48:52 -0600

    buildscript changes

--- buildScript.sh
+++ buildScript.sh
@@ -1,27 +1,22 @@
 #!/usr/bin/env bash
 
+# This build script will build a new docker container with a specified version tag.
+
+# Do not use latest tag to avoid complications. instead simply version the application
+
 # Build a new docker image
-# do NOT use tags, then push WITH latest tag !important
 docker build -t us.gcr.io/sound-mob12282/soundmob:15.02 .
 
 # Push new docker image to gcloud
 docker push us.gcr.io/sound-mob12282/soundmob:15.02
 
+# Navigate to GCLOUD
+# Create a new GCE with recent docker image
+# SSH into NGINX proxy
+# Redirect to new GCE internal IP
+# Wait for compile, and the new version of the application should be running at soundmob.net
 
+
+## currently buggy
 # Update running compute engine with newest image
 # gcloud compute instanceslatest update-container soundmobmachine7 --container-image us.gcr.io/sound-mob12282/soundmob:
-
-
-#server.listen change to 80
-#let googleCallbackURL = 'localhost:3000/auth/google/callback';
-# if (process.env.NODEENV === 'production') {
-#   googleCallbackURL = 'soundmob.net/auth/google/callback';
-# }
-
-#certbot shit, this will give us a cert that lasts 3 months
-# certbot -d soundmob.net --manual --logs-dir certbot --config-dir certbot --work-dir certbot --server https://acme-v02.api.letsencrypt.org/directory --email jvalamis@gmail.com --preferred-challenges dns certonly
-
-#checking if domain
-#dig @8.8.8.8 soundmob.systems
-#host -t txt soundmob.systems
-#nslookup -type=txt soundmob.systems
